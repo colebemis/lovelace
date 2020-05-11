6d66c61b676c58435141e747c87595ee84a3b237
Cole Bemis <colebemis@github.com>
2020-05-10 21:57:14 -0700

    Create Node component

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,35 @@
 import React from 'react'
 
-function App() {
-  return <div>Hello world</div>
+export default function App() {
+  return (
+    <div>
+      <Node id="stateA" x={100} y={200}></Node>
+      <Node id="stateB" x={200} y={400}></Node>
+    </div>
+  )
 }
 
-export default App
+interface NodeProps {
+  id: string
+  x: number
+  y: number
+}
+
+function Node({ id, x, y }: NodeProps) {
+  return (
+    <div
+      id={id}
+      style={{
+        position: 'absolute',
+        top: y,
+        left: x,
+        transform: 'translate(-50%, -50%)',
+        padding: 16,
+        border: '2px solid',
+        borderRadius: 6,
+      }}
+    >
+      ({x}, {y})
+    </div>
+  )
+}
